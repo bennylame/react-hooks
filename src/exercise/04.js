e9245eccc6614e2a6deb42c7d1f11420222faebd
Benny Lam <bennyvlam@gmail.com>
2022-12-22 18:00:46 +0100

    done(ep4): finished

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,10 +2,12 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import React from 'react'
+import {useState} from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  // const squares = Array(9).fill(null)
+  const [squares, setSquares] = useState(Array(9).fill(null))
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -13,6 +15,9 @@ function Board() {
   // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
   // 💰 I've written the calculations for you! So you can use my utilities
   // below to create these variables
+  let nextValue = calculateNextValue(squares)
+  let winner = calculateWinner(squares)
+  let status = calculateStatus(winner, squares, nextValue)
 
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
@@ -20,7 +25,10 @@ function Board() {
     // 🐨 first, if there's already winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
-    //
+    if (calculateWinner(squares) || squares[square]) return
+    let copySquares = [...squares]
+    copySquares[square] = nextValue
+    setSquares(copySquares)
     // 🦉 It's typically a bad idea to manipulate state in React because that
     // can lead to subtle bugs that can easily slip into productions.
     // 🐨 make a copy of the squares array (💰 `[...squares]` will do it!)
@@ -32,6 +40,7 @@ function Board() {
 
   function restart() {
     // 🐨 set the squares to `Array(9).fill(null)`
+    setSquares(Array(9).fill(null))
   }
 
   function renderSquare(i) {
@@ -45,7 +54,7 @@ function Board() {
   return (
     <div>
       {/* 🐨 put the status here */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
