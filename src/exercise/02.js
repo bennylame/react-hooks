89b7411096bddeb5f325bda6308a1ae63cdf8fd8
Benny Lam <bennyvlam@gmail.com>
2022-12-22 17:04:50 +0100

    done(ep2): flexible custom hook, HARD task af

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,20 +1,18 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
+import useLocalStorageState from './useLocalStorageState'
 import React from 'react'
+import {useState} from 'react'
 
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
-
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  const [name, setName] = useLocalStorageState('name', initialName)
+  // const [name, setName] = useState(initialName)
 
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
@@ -27,7 +25,11 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return (
+    <>
+      <Greeting />
+    </>
+  )
 }
 
 export default App
